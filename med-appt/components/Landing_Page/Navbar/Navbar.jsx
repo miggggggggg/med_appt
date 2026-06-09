0666f11b86c561f9222b15fba4581799fbaf5da4
Miguel Baldacchino <ntgmiguel543@gmail.com>
2026-06-09 10:16:19 +0200

    Add necesarry files

---
+++ med-appt/components/Landing_Page/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import "../Navbar/Navbar.css"
+
+function Navbar(){
+
+    return(<>
+    <div className="navbarContainer">
+      <h2><a className="anchorLink" href="#stayhealthy">StayHealthy</a></h2>
+      <nav>
+        <ul className="navbar">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#appointments">Appointments</a></li>
+          <li><a href="#signUp">Sign up</a></li>
+          <li><a href="#login">Login</a></li>
+        </ul>
+      </nav>
+    </div>
+    </>)
+}
--- med-appt/src/App.jsx
+++ med-appt/src/App.jsx
@@ -3,12 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "../components/Landing_Page/Navbar/Navbar"
 function App() {
 
 
   return (
    
-      <></>
+      <>
+       <div className="App">
+        {/* Set up BrowserRouter for routing */}
+        <BrowserRouter>
+          {/* Display the Navbar component */}
+          <Navbar/>
+
+          {/* Set up the Routes for different pages */}
+          <Routes>
+            {/* Define individual Route components for different pages */}
+          </Routes>
+        </BrowserRouter>
+    </div>
+    </>
   )
 }
+
+export default App;
